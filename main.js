e44cab302720ce053c85171d2f2f304608e7341e
KaskaS-O <kaska.salaban@gmail.com>
2021-10-13 13:15:54 +0200

    Create desktop layout and active state, change html sctucture of .share component

--- main.js
+++ main.js
@@ -4,14 +4,20 @@ const shareSocials = document.querySelector('.share--socials');
 const authorEl = document.querySelector('.author');
 const socialsEl = document.querySelector('.socials');
 
+const squareEl = document.querySelector('.socials__square');
+
 const handleShareAuthor = () => {
     authorEl.style = 'display:none';
     socialsEl.style = 'display:grid';
+    shareSocials.style = 'display:block';
+    squareEl.style = 'display:block';
 };
 
 const handleShareSocials = () => {
     socialsEl.style = 'display:none';
     authorEl.style = 'display:grid';
+    shareSocials.style = 'display:none';
+    squareEl.style = 'display:none';
 }
 
 shareAuthor.addEventListener('pointerdown', handleShareAuthor);
